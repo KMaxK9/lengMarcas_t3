1c5e067b7f19ff950f78eb240bb54dc892c64950
Maximilian K <kmaxkogler@gmail.com>
2023-05-04 12:50:23 +0200

    C - tateti 27/04

--- ej.27_04/dynamic.js
+++ ej.27_04/dynamic.js
@@ -1,50 +1,64 @@
 'use strict'
 // código de alumno: Maximilian Kögler || fecha: 27 Abril 2023.
 
-const board = new Array([0,0,0],[0,0,0],[0,0,0]);
-var winSquare = false;
-var winCircle = false;
+let clicked = [];
+let boardBlocked = false;
+let current_player = "X";
+let board = ['', '', '', '', '', '', '', '', ''];
 
+// Función responsable de cambiar el color al mover el ratón por encima de una casilla.
 function enter(id) {
-    let elem = document.getElementById(id);
-    elem.style.backgroundColor = "rgb(200, 200, 200)";
+  let elem = document.getElementById(id);
+  elem.style.backgroundColor = "rgb(200, 200, 200)";
 } // enter
 
+// Función responsable de cambiar el color al quitar el ratón de encima de una casilla.
 function leave(id) {
-    let elem = document.getElementById(id);
-    elem.style.backgroundColor = "rgba(255, 255, 255, 0)";
+  let elem = document.getElementById(id);
+  elem.style.backgroundColor = "rgba(255, 255, 255, 0)";
 } // leave
 
-function markCircle(id,x,y) {
-    let elem = document.getElementById(id);
-    elem.innerHTML = "<div style='border-style: solid; border-color:lime; border-radius: 50%; height: 10vh; width: 10vw;'></div>";
-    board(x,y) = 1;
-} // circle
+// mark(id), nos sirve para "marcar" las casillas a la que pinchamos.
+function mark(ident) {
+  document.getElementById(ident).innerHTML = (current_player);
 
-function markSquare(id,x,y) {
-    let elem = document.getElementById(id);
-    elem.innerHTML = "<div style='border-style: solid; border-color:red; height: 10vh; width: 10vw;'></div>";
-    board(x,y) = 2;
-} // square
+  // Marcamos la casilla con el símbolo del jugador actual
+  var casilla = ident.substring(1, 2) - 1;
+  board[casilla] = current_player;
 
-function check() {
-    var points1 = 0;
-    var points2 = 0;
-    for(let x = 0 ; x < board.length ; x++) {
-        for(let y = 0 ; y < board(x).length ; y ++) {
-            if (board(x,y) == 1) {
-                points1++;
-            } // if
-            if(points1 <= 3) {
-                winCircle = true;
-            }
-        } // for
-        if (winCircle || winSquare) {
-            throw new Error("Has ganado!");
-        } // if
-        points1 = 0;
-        points2 = 0;
-    } // for
-} // comp
+  // Verificar si hay un ganador
+  const winner = checkWinner();
+  if (winner) {
+    alert("¡" + winner + " ha ganado!");
+    document.getElementById("ganador").innerHTML = ("¡Ha ganado " + winner + "!");
+    return;
+  } // if
+  // Cambia de jugador.
+  current_player = current_player == 'X' ? 'O' : 'X';
+} // mark
 
-check();
+// Función que comprueba si un jugador ha ganado.
+function checkWinner() {
+  // Combinaciones posibles para ganar.
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  // Bucle que recorre las combinaciones posibles en el array (board).
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+    if (board[a] == board[b] && board[b] == board[c] && board[a] != '') {
+      return board[a];
+    } // if
+  } // for
+  return null;
+} // checkWinner
+
+
+
